e3c74ac3be915aad0f7bda771a12e04e434119ce
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2019-10-09 00:18:38 +0900

    fix: remove xo

--- lib/index.spec.ts
+++ lib/index.spec.ts
@@ -1,7 +1,7 @@
 import helloWorld from '.'
 
 test('test of hello world', () => {
-  const name = 'abyssparanoia'
-  const result = helloWorld(name)
-  expect(result).toBe(`Hello World! Mr.${name}`)
+	const name = 'abyssparanoia'
+	const result = helloWorld(name)
+	expect(result).toBe(`Hello World! Mr.${name}`)
 })
