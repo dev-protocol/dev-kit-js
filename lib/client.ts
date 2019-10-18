a12f29a32e8b2651968a8019d5bcc211c3e151fd
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2019-10-19 04:13:34 +0900

    feat: impl schema

--- lib/client.ts
+++ lib/client.ts
@@ -1,8 +1,15 @@
 import Web3 from 'web3'
 
-export const DevClient = (host: string, timeout?: number): Web3 => {
+export const devClient = (host: string, timeout?: number): Web3 => {
 	const client = new Web3()
 
 	client.setProvider(new Web3.providers.HttpProvider(host, timeout))
 	return client
+}
+
+export interface CustomOptions {
+	from?: string
+	gasPrice?: string
+	gas: number
+	data: string
 }
