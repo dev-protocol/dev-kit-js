5d3797875e0e4021e5d2dc2e5d0e666b4200a27e
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2019-10-19 14:53:13 +0900

    feat: impl schema

--- lib/market.ts
+++ lib/market.ts
@@ -1,53 +1,18 @@
 import { devClient, CustomOptions } from './client'
-
-const marketABI = [
-	{
-		type: 'constructor',
-		payable: false,
-		stateMutability: 'nonpayable',
-		inputs: [{ name: 'testInt', type: 'uint256' }]
-	},
-	{
-		type: 'function',
-		name: 'foo',
-		constant: false,
-		payable: false,
-		stateMutability: 'nonpayable',
-		inputs: [{ name: 'b', type: 'uint256' }, { name: 'c', type: 'bytes32' }],
-		outputs: [{ name: '', type: 'address' }]
-	},
-	{
-		type: 'event',
-		name: 'Event',
-		inputs: [
-			{ indexed: true, name: 'b', type: 'uint256' },
-			{ indexed: false, name: 'c', type: 'bytes32' }
-		],
-		anonymous: false
-	},
-	{
-		type: 'event',
-		name: 'Event2',
-		inputs: [
-			{ indexed: true, name: 'b', type: 'uint256' },
-			{ indexed: false, name: 'c', type: 'bytes32' }
-		],
-		anonymous: false
-	}
-]
+import { marketAbi } from './market-abi'
 
 const marketContract = (host: string, timeout?: number) => (
 	address?: string,
 	options?: CustomOptions
 ) =>
-	new (devClient(host, timeout)).eth.Contract(marketABI, address, {
+	new (devClient(host, timeout)).eth.Contract(marketAbi, address, {
 		...options
 	})
 
 export const schema = (host: string, timeout?: number) => (
 	address?: string,
 	options?: CustomOptions
-) => async (): Promise<{ memory: string }> =>
+) => async (): Promise<{ 0: string }> =>
 	marketContract(host, timeout)(address, options)
 		.methods.schema()
 		.call()
