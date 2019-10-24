a6b365abf936428cb34be6d1bfbc791d4a8c567c
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2019-10-24 20:10:04 +0900

    feat: add schema test

--- lib/market/schema.spec.ts
+++ lib/market/schema.spec.ts
@@ -28,5 +28,49 @@ describe('schema.ts', () => {
 
 			expect(JSON.stringify(result)).toEqual(JSON.stringify(expected))
 		})
+
+		it('call success', async () => {
+			const value = `["aaa","bbbb","cccc"]`
+
+			const marketContract = {
+				methods: {
+					schema: () => ({
+						call: jest
+							.fn()
+							.mockImplementation(async () => Promise.resolve(value))
+					})
+				}
+			}
+
+			const expected = JSON.parse(value)
+
+			// eslint-disable-next-line @typescript-eslint/no-explicit-any
+			const caller = createSchemaCaller(marketContract as any)
+
+			const result = await caller()
+
+			expect(result).toEqual(expected)
+		})
+
+		it('call failure', async () => {
+			const error = 'error'
+
+			const marketContract = {
+				methods: {
+					schema: () => ({
+						call: jest
+							.fn()
+							.mockImplementation(async () => Promise.reject(error))
+					})
+				}
+			}
+
+			// eslint-disable-next-line @typescript-eslint/no-explicit-any
+			const caller = createSchemaCaller(marketContract as any)
+
+			const result = await caller().catch(err => err)
+
+			expect(result).toEqual(error)
+		})
 	})
 })
