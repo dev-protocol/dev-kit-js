db10792912567dbc7d5466cb531b9e67a7ba4e05
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2019-10-24 20:38:54 +0900

    fix: path

--- lib/client.ts
+++ lib/client.ts
@@ -1,5 +1,5 @@
 import Web3 from 'web3'
-import { createMarketContract } from './market'
+import { createMarketContract } from './market/index'
 
 export interface DevkitContract {
 	market: ReturnType<typeof createMarketContract>
