a4e4933520d7fac0559fd9e11fe25c58a28da4e5
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2019-10-26 22:26:42 +0900

    feat: impl calculate caller

---
+++ lib/market/calculate.ts
@@ -0,0 +1,13 @@
+import Contract from 'web3/eth/contract'
+
+export const createCalculateCaller = (
+	contract: Contract
+): ((
+	metrics: string,
+	start: string,
+	end: string
+) => Promise<boolean>) => async (metrics: string, start: string, end: string) =>
+	contract.methods
+		.schema([metrics, start, end])
+		.call()
+		.then(result => result as boolean)
