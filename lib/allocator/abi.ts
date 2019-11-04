0add0ce9522d00d9d527fbb017c8a4e50fb10671
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2019-11-04 19:58:45 +0900

    feat: add abi

---
+++ lib/allocator/abi.ts
@@ -0,0 +1,314 @@
+export const allocatorAbi = [
+	{
+		anonymous: false,
+		inputs: [
+			{
+				indexed: true,
+				internalType: 'address',
+				name: 'previousOwner',
+				type: 'address'
+			},
+			{
+				indexed: true,
+				internalType: 'address',
+				name: 'newOwner',
+				type: 'address'
+			}
+		],
+		name: 'OwnershipTransferred',
+		type: 'event'
+	},
+	{
+		constant: true,
+		inputs: [],
+		name: '_owner',
+		outputs: [
+			{
+				internalType: 'address payable',
+				name: '',
+				type: 'address'
+			}
+		],
+		payable: false,
+		stateMutability: 'view',
+		type: 'function'
+	},
+	{
+		constant: false,
+		inputs: [
+			{
+				internalType: 'address',
+				name: '_token',
+				type: 'address'
+			},
+			{
+				internalType: 'address',
+				name: '_from',
+				type: 'address'
+			},
+			{
+				internalType: 'address',
+				name: '_to',
+				type: 'address'
+			}
+		],
+		name: 'beforeBalanceChange',
+		outputs: [],
+		payable: false,
+		stateMutability: 'nonpayable',
+		type: 'function'
+	},
+	{
+		constant: false,
+		inputs: [
+			{
+				internalType: 'address',
+				name: 'nextState',
+				type: 'address'
+			}
+		],
+		name: 'changeStateAddress',
+		outputs: [],
+		payable: false,
+		stateMutability: 'nonpayable',
+		type: 'function'
+	},
+	{
+		constant: true,
+		inputs: [],
+		name: 'initialPaymentBlock',
+		outputs: [
+			{
+				internalType: 'uint256',
+				name: '',
+				type: 'uint256'
+			}
+		],
+		payable: false,
+		stateMutability: 'view',
+		type: 'function'
+	},
+	{
+		constant: true,
+		inputs: [],
+		name: 'isOwner',
+		outputs: [
+			{
+				internalType: 'bool',
+				name: '',
+				type: 'bool'
+			}
+		],
+		payable: false,
+		stateMutability: 'view',
+		type: 'function'
+	},
+	{
+		constant: false,
+		inputs: [],
+		name: 'kill',
+		outputs: [],
+		payable: false,
+		stateMutability: 'nonpayable',
+		type: 'function'
+	},
+	{
+		constant: true,
+		inputs: [],
+		name: 'lastPaymentBlock',
+		outputs: [
+			{
+				internalType: 'uint256',
+				name: '',
+				type: 'uint256'
+			}
+		],
+		payable: false,
+		stateMutability: 'view',
+		type: 'function'
+	},
+	{
+		constant: true,
+		inputs: [],
+		name: 'lastTotalAllocationValuePerBlock',
+		outputs: [
+			{
+				internalType: 'uint256',
+				name: '',
+				type: 'uint256'
+			}
+		],
+		payable: false,
+		stateMutability: 'view',
+		type: 'function'
+	},
+	{
+		constant: true,
+		inputs: [],
+		name: 'mintPerBlock',
+		outputs: [
+			{
+				internalType: 'uint256',
+				name: '',
+				type: 'uint256'
+			}
+		],
+		payable: false,
+		stateMutability: 'view',
+		type: 'function'
+	},
+	{
+		constant: true,
+		inputs: [],
+		name: 'owner',
+		outputs: [
+			{
+				internalType: 'address',
+				name: '',
+				type: 'address'
+			}
+		],
+		payable: false,
+		stateMutability: 'view',
+		type: 'function'
+	},
+	{
+		constant: false,
+		inputs: [],
+		name: 'renounceOwnership',
+		outputs: [],
+		payable: false,
+		stateMutability: 'nonpayable',
+		type: 'function'
+	},
+	{
+		constant: true,
+		inputs: [],
+		name: 'totalPaymentValue',
+		outputs: [
+			{
+				internalType: 'uint256',
+				name: '',
+				type: 'uint256'
+			}
+		],
+		payable: false,
+		stateMutability: 'view',
+		type: 'function'
+	},
+	{
+		constant: false,
+		inputs: [
+			{
+				internalType: 'address',
+				name: 'newOwner',
+				type: 'address'
+			}
+		],
+		name: 'transferOwnership',
+		outputs: [],
+		payable: false,
+		stateMutability: 'nonpayable',
+		type: 'function'
+	},
+	{
+		constant: false,
+		inputs: [
+			{
+				internalType: 'address',
+				name: '_token',
+				type: 'address'
+			}
+		],
+		name: 'withdraw',
+		outputs: [],
+		payable: true,
+		stateMutability: 'payable',
+		type: 'function'
+	},
+	{
+		constant: false,
+		inputs: [
+			{
+				internalType: 'uint256',
+				name: '_sec',
+				type: 'uint256'
+			}
+		],
+		name: 'setSecondsPerBlock',
+		outputs: [],
+		payable: false,
+		stateMutability: 'nonpayable',
+		type: 'function'
+	},
+	{
+		constant: false,
+		inputs: [
+			{
+				internalType: 'uint256',
+				name: '_value',
+				type: 'uint256'
+			}
+		],
+		name: 'updateAllocateValue',
+		outputs: [],
+		payable: false,
+		stateMutability: 'nonpayable',
+		type: 'function'
+	},
+	{
+		constant: false,
+		inputs: [
+			{
+				internalType: 'address',
+				name: '_metrics',
+				type: 'address'
+			}
+		],
+		name: 'allocate',
+		outputs: [],
+		payable: true,
+		stateMutability: 'payable',
+		type: 'function'
+	},
+	{
+		constant: false,
+		inputs: [
+			{
+				internalType: 'address',
+				name: '_metrics',
+				type: 'address'
+			},
+			{
+				internalType: 'uint256',
+				name: '_value',
+				type: 'uint256'
+			}
+		],
+		name: 'calculatedCallback',
+		outputs: [],
+		payable: false,
+		stateMutability: 'nonpayable',
+		type: 'function'
+	},
+	{
+		constant: false,
+		inputs: [
+			{
+				internalType: 'address',
+				name: '_property',
+				type: 'address'
+			},
+			{
+				internalType: 'uint256',
+				name: '_amount',
+				type: 'uint256'
+			}
+		],
+		name: 'investToProperty',
+		outputs: [],
+		payable: false,
+		stateMutability: 'nonpayable',
+		type: 'function'
+	}
+]
