fba850b5cbe28dbb7793a97f180fe950ca735b84
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2019-11-11 09:23:50 +0900

    feat: impl create allocator client

---
+++ lib/allocator/index.ts
@@ -0,0 +1,29 @@
+import Web3 from 'web3'
+import Contract from 'web3/eth/contract'
+import { allocatorAbi } from './abi'
+import { CustomOptions } from '../option'
+import { createAllocateCaller } from './allocate'
+import { createWithdrawCaller } from './withdraw'
+
+export interface CreateAllocatorContract {
+	allocate: (address: string) => Promise<void>
+	withdraw: (address: string) => Promise<void>
+}
+
+export const createAllocatorContract = (client: Web3) => (
+	address?: string,
+	options?: CustomOptions
+): CreateAllocatorContract => {
+	const contractClient: Contract = new client.eth.Contract(
+		allocatorAbi,
+		address,
+		{
+			...options
+		}
+	)
+
+	return {
+		allocate: createAllocateCaller(contractClient),
+		withdraw: createWithdrawCaller(contractClient)
+	}
+}
