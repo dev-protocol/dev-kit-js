655607e8c20e02a84ac83ec1a8f878de3a70d79f
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2019-11-11 09:16:37 +0900

    feat: impl createWithdrawCaller

---
+++ lib/allocator/withdraw.ts
@@ -0,0 +1,9 @@
+import Contract from 'web3/eth/contract'
+
+export const createWithdrawCaller = (contract: Contract) => async (
+	address: string
+) =>
+	contract.methods
+		.withdraw(address)
+		.call()
+		.then(result => result as string)
