2061278eecee62be6bb7bef0e879fafba1fb4242
aggre <hiroyuki.aggre@gmail.com>
2020-05-25 00:07:24 +0900

    rewrite function so that it does not need to disable eslint

--- lib/utils/getAccount.ts
+++ lib/utils/getAccount.ts
@@ -3,11 +3,12 @@ import Web3 from 'web3'
 const map: WeakMap<Web3, string> = new WeakMap()
 
 export const getAccount = async (web3: Web3): Promise<string> =>
-	(async (res) =>
+	(async (res: string | undefined): Promise<string> =>
 		typeof res === 'string'
 			? res
-			: web3.eth.getAccounts().then(([account]) => {
-					// eslint-disable-next-line functional/no-expression-statement
-					map.set(web3, account)
-					return account
-			  }))(map.get(web3))
+			: web3.eth
+					.getAccounts()
+					.then(
+						([account]: readonly string[]) =>
+							map.set(web3, account).get(web3) as string
+					))(map.get(web3))
