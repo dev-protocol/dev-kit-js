950e83f88ce9959b1e77aa30dd823326c544c2e3
aggre <hiroyuki.aggre@gmail.com>
2020-12-28 14:51:51 +0900

    build file for cjs uses es5

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,36 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve'
 import multi from '@rollup/plugin-multi-entry'
+import { getBabelOutputPlugin } from '@rollup/plugin-babel'
 import dts from 'rollup-plugin-dts'
+
+const input = ['dist/lib/*.js', 'dist/lib/**/index.js', '!**/*.spec.*']
+const plugins = [multi(), nodeResolve({ modulesOnly: true })]
 
 export default [
 	{
-		input: ['dist/lib/*.js', 'dist/lib/**/index.js', '!**/*.spec.*'],
+		input,
 		output: [
 			{
 				file: 'dist/index.mjs',
 				format: 'es',
 			},
+		],
+		plugins,
+	},
+	{
+		input,
+		output: [
 			{
 				file: 'dist/index.js',
 				format: 'cjs',
 			},
 		],
-		plugins: [multi(), nodeResolve({ modulesOnly: true })],
+		plugins: [
+			...plugins,
+			getBabelOutputPlugin({
+				presets: ['@babel/preset-env'],
+			}),
+		],
 	},
 	{
 		input: ['dist/lib/*.d.ts', 'dist/lib/**/index.d.ts', '!**/*.spec.*'],
