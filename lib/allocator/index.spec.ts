7274602a45cbeefa9ef4f41665fc158641d86016
k3nt0w <kento.watanabe.11@gmail.com>
2021-08-15 14:12:42 +0900

    fix: mock calculateMaxRewardsPerBlock function

--- lib/allocator/index.spec.ts
+++ lib/allocator/index.spec.ts
@@ -3,8 +3,13 @@ import { createAllocatorContract, CreateAllocatorContract } from '.'
 import { createCalculateMaxRewardsPerBlockCaller } from './calculateMaxRewardsPerBlock'
 import { allocatorAbi } from './abi'
 
+jest.mock('./calculateMaxRewardsPerBlock')
+
 describe('allocator/index.ts', () => {
 	describe('createAllocatorContract', () => {
+		;(createCalculateMaxRewardsPerBlockCaller as jest.Mock).mockImplementation(
+			(contract) => contract
+		)
 		it('check return object', () => {
 			const host = 'localhost'
 			const address = 'address'
@@ -12,20 +17,19 @@ describe('allocator/index.ts', () => {
 			const expected: (address: string) => CreateAllocatorContract = (
 				address: string
 			) => {
-				const allocatorContract = new ethers.Contract(
+				const contract = new ethers.Contract(
 					address,
 					[...allocatorAbi],
 					provider
 				)
 				return {
 					calculateMaxRewardsPerBlock: createCalculateMaxRewardsPerBlockCaller(
-						allocatorContract
+						contract
 					),
 				}
 			}
 
 			const result = createAllocatorContract(provider)
-
 			expect(JSON.stringify(result)).toEqual(JSON.stringify(expected))
 			expect(JSON.stringify(result(address))).toEqual(
 				JSON.stringify(expected(address))
