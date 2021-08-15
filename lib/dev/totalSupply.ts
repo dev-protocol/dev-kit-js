f901df6e562a9771e4997cddbb9a2c75b8a1f05a
k3nt0w <kento.watanabe.11@gmail.com>
2021-08-15 14:42:09 +0900

    feat: replace total supply

--- lib/dev/totalSupply.ts
+++ lib/dev/totalSupply.ts
@@ -1,11 +1,14 @@
-import { Contract } from 'web3-eth-contract/types'
-import { execute } from '../utils/execute'
+import { ethers } from 'ethers'
+import { execute, QueryOption } from '../utils/ethers-execute'
 import { always } from 'ramda'
 
 export type CreateTotalSupplyCaller = (
-	contract: Contract
+	contract: ethers.Contract
 ) => () => Promise<string>
 
 export const createTotalSupplyCaller: CreateTotalSupplyCaller = (
-	contract: Contract
-) => always(execute({ contract, method: 'totalSupply' }))
+	contract: ethers.Contract
+) =>
+	always(
+		execute<QueryOption>({ contract, method: 'totalSupply', mutation: false })
+	)
