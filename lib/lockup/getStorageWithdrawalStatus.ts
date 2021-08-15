7cde3dca1252c5211e37d231fb1f5ee545f30e55
k3nt0w <kento.watanabe.11@gmail.com>
2021-08-15 18:53:15 +0900

    feat: createGetStorageWithdrawalStatusCaller

--- lib/lockup/getStorageWithdrawalStatus.ts
+++ lib/lockup/getStorageWithdrawalStatus.ts
@@ -1,15 +1,16 @@
-import { Contract } from 'web3-eth-contract/types'
-import { execute } from '../utils/execute'
+import { ethers } from 'ethers'
+import { execute, QueryOption } from '../utils/ethers-execute'
 
 export type CreateGetStorageWithdrawalStatusCaller = (
-	contract: Contract
+	contract: ethers.Contract
 ) => (propertyAddress: string, accountAddress: string) => Promise<string>
 
 export const createGetStorageWithdrawalStatusCaller: CreateGetStorageWithdrawalStatusCaller = (
-	contract: Contract
+	contract: ethers.Contract
 ) => async (propertyAddress: string, accountAddress: string) =>
-	execute({
+	execute<QueryOption>({
 		contract,
 		method: 'getStorageWithdrawalStatus',
 		args: [propertyAddress, accountAddress],
+		mutation: false,
 	})
