b33244ac83ff1d85ae3c18ec68e00d99c305863a
k3nt0w <kento.watanabe.11@gmail.com>
2021-08-17 22:45:15 +0900

    feat: dev

--- lib/dev/index.spec.ts
+++ lib/dev/index.spec.ts
@@ -1,4 +1,4 @@
-import Web3 from 'web3'
+import { ethers } from 'ethers'
 import { createDevContract, DevContract } from '.'
 import { createTransferCaller } from './../erc20/transfer'
 import { devAbi } from './abi'
@@ -13,42 +13,64 @@ import { createSymbolCaller } from './../erc20/symbol'
 import { createDecimalsCaller } from './../erc20/decimals'
 import { createAllowanceCaller } from './../erc20/allowance'
 
+jest.mock('./deposit')
+jest.mock('./../erc20/balanceOf')
+jest.mock('./../erc20/totalSupply')
+jest.mock('./../erc20/approve')
+jest.mock('../erc20/transferFrom')
+jest.mock('./../erc20/name')
+jest.mock('./../erc20/symbol')
+jest.mock('./../erc20/decimals')
+jest.mock('./../erc20/allowance')
+
 describe('dev/index.ts', () => {
+	;(createDepositCaller as jest.Mock).mockImplementation((contract) => contract)
+	;(createBalanceOfCaller as jest.Mock).mockImplementation(
+		(contract) => contract
+	)
+	;(createTotalSupplyCaller as jest.Mock).mockImplementation(
+		(contract) => contract
+	)
+	;(createApproveCaller as jest.Mock).mockImplementation((contract) => contract)
+	;(createTransferFromCaller as jest.Mock).mockImplementation(
+		(contract) => contract
+	)
+	;(createNameCaller as jest.Mock).mockImplementation((contract) => contract)
+	;(createSymbolCaller as jest.Mock).mockImplementation((contract) => contract)
+	;(createDecimalsCaller as jest.Mock).mockImplementation(
+		(contract) => contract
+	)
+	;(createAllowanceCaller as jest.Mock).mockImplementation(
+		(contract) => contract
+	)
+
 	describe('createDevContract', () => {
 		it('check return object', () => {
 			const host = 'localhost'
-			const client = new Web3()
-			client.setProvider(new Web3.providers.HttpProvider(host))
-
-			const expected: (
-				address?: string,
-				options?: CustomOptions
-			) => DevContract = (address?: string, options?: CustomOptions) => {
-				const devContract = new client.eth.Contract([...devAbi], address, {
-					...options,
-				})
+			const address = '0x0000000000000000000000000000000000000000'
+			const provider = new ethers.providers.JsonRpcProvider(host)
+			const expected: (address: string) => DevContract = (address: string) => {
+				const contract = new ethers.Contract(address, [...devAbi], provider)
 				return {
-					totalSupply: createTotalSupplyCaller(devContract),
-					balanceOf: createBalanceOfCaller(devContract),
-					transfer: createTransferCaller(devContract, client),
-					allowance: createAllowanceCaller(devContract),
-					approve: createApproveCaller(devContract, client),
-					transferFrom: createTransferFromCaller(devContract, client),
-					name: createNameCaller(devContract),
-					symbol: createSymbolCaller(devContract),
-					decimals: createDecimalsCaller(devContract),
-					deposit: createDepositCaller(devContract, client),
-					contract: () => devContract,
+					totalSupply: createTotalSupplyCaller(contract),
+					balanceOf: createBalanceOfCaller(contract),
+					transfer: createTransferCaller(contract),
+					allowance: createAllowanceCaller(contract),
+					approve: createApproveCaller(contract),
+					transferFrom: createTransferFromCaller(contract),
+					name: createNameCaller(contract),
+					symbol: createSymbolCaller(contract),
+					decimals: createDecimalsCaller(contract),
+					deposit: createDepositCaller(contract),
+					contract: () => contract,
 				}
 			}
 
-			const result = createDevContract(client)
+			const result = createDevContract(provider)
 
 			expect(JSON.stringify(result)).toEqual(JSON.stringify(expected))
-			expect(
-				JSON.stringify(result('0x0000000000000000000000000000000000000000'))
-			).toEqual(
-				JSON.stringify(expected('0x0000000000000000000000000000000000000000'))
+			expect(JSON.stringify(result(address))).toEqual(
+				JSON.stringify(expected(address))
 			)
 		})
 	})
