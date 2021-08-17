39e7cbb0abbff8f86d8031fe32e9c2257e6323e6
k3nt0w <kento.watanabe.11@gmail.com>
2021-08-17 22:44:41 +0900

    feat: lib erc20

--- lib/erc20/balanceOf.spec.ts
+++ lib/erc20/balanceOf.spec.ts
@@ -7,14 +7,12 @@ describe('balanceOf.spec.ts', () => {
 			const value = '12345'
 
 			const contract = {
-				methods: {
+				balanceOf: jest
+					.fn()
 					// eslint-disable-next-line @typescript-eslint/no-unused-vars
-					balanceOf: (address: string) => ({
-						call: jest
-							.fn()
-							.mockImplementation(async () => Promise.resolve(value)),
-					}),
-				},
+					.mockImplementation(async (address: string) =>
+						Promise.resolve(value)
+					),
 			}
 
 			const expected = value
@@ -32,14 +30,10 @@ describe('balanceOf.spec.ts', () => {
 			const address = '0x0472ec0185ebb8202f3d4ddb0226998889663cf2'
 
 			const contract = {
-				methods: {
+				balanceOf: jest
+					.fn()
 					// eslint-disable-next-line @typescript-eslint/no-unused-vars
-					balanceOf: (address: string) => ({
-						call: jest
-							.fn()
-							.mockImplementation(async () => Promise.reject(error)),
-					}),
-				},
+					.mockImplementation(async (address: string) => Promise.reject(error)),
 			}
 
 			// eslint-disable-next-line @typescript-eslint/no-explicit-any
--- lib/erc20/transfer.ts
+++ lib/erc20/transfer.ts
@@ -1,20 +1,16 @@
-/* eslint-disable @typescript-eslint/prefer-readonly-parameter-types */
-import { Contract } from 'web3-eth-contract/types'
-import Web3 from 'web3'
-import { execute } from '../utils/execute'
+import { ethers } from 'ethers'
+import { execute, MutationOption } from '../utils/ethers-execute'
 import { T } from 'ramda'
 
 export type CreateTransferCaller = (
-	contract: Contract,
-	client: Web3
+	contract: ethers.Contract
 ) => (to: string, value: string) => Promise<boolean>
 
 export const createTransferCaller: CreateTransferCaller =
-	(contract: Contract, client: Web3) => async (to: string, value: string) =>
-		execute({
+	(contract: ethers.Contract) => async (to: string, value: string) =>
+		execute<MutationOption>({
 			contract,
 			method: 'transfer',
-			mutation: true,
-			client,
 			args: [to, value],
+			mutation: true,
 		}).then(T)
