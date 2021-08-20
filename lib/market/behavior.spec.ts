feeb7044dd7e850ffd810da24d1deea65d305eb9
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2021-08-20 16:46:19 +0900

    fix: test

--- lib/market/behavior.spec.ts
+++ lib/market/behavior.spec.ts
@@ -6,13 +6,7 @@ describe('behavior.spec.ts', () => {
 			const value = '0x0000.........'
 
 			const contract = {
-				methods: {
-					behavior: () => ({
-						call: jest
-							.fn()
-							.mockImplementation(async () => Promise.resolve(value)),
-					}),
-				},
+				behavior: () => Promise.resolve(value),
 			}
 
 			const expected = value
@@ -26,24 +20,16 @@ describe('behavior.spec.ts', () => {
 		})
 
 		it('call failure', async () => {
-			const error = 'error'
+			const err = new Error('error')
 
 			const contract = {
-				methods: {
-					behavior: () => ({
-						call: jest
-							.fn()
-							.mockImplementation(async () => Promise.reject(error)),
-					}),
-				},
+				behavior: () => Promise.reject(err),
 			}
 
 			// eslint-disable-next-line @typescript-eslint/no-explicit-any
 			const caller = createBehaviorCaller(contract as any)
 
-			const result = await caller().catch((err) => err)
-
-			expect(result).toEqual(error)
+			await expect(caller()).rejects.toThrowError(err)
 		})
 	})
 })
