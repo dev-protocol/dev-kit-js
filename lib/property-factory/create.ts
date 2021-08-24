32bf30f55aa9e68e06827bcc9a850e93eb30584e
k3nt0w <kento.watanabe.11@gmail.com>
2021-08-24 14:04:49 +0900

    Revert "feat: add checking contract address process"
    This reverts commit bdf82c1775798b275ca08a5cd31f2d9413ee7dcf.

--- lib/property-factory/create.ts
+++ lib/property-factory/create.ts
@@ -1,8 +1,6 @@
 /* eslint-disable @typescript-eslint/prefer-readonly-parameter-types */
 /* eslint-disable functional/no-expression-statement */
-/* eslint-disable functional/no-conditional-statement */
 import { ethers } from 'ethers'
-import { reject } from 'ramda'
 import { execute, MutationOption } from '../utils/execute'
 
 export type CreateCreatePropertyCaller = (
@@ -19,17 +17,9 @@ export const createCreatePropertyCaller: CreateCreatePropertyCaller =
 			mutation: true,
 		})
 
-		return new Promise((resolve, reject) => {
-			const subscriberdContract = contract.on(
-				'Create',
-				async (contractAddress: string, propertyAddress: string) => {
-					if (contract.address === contractAddress) {
-						subscriberdContract.removeAllListeners()
-						resolve(propertyAddress)
-					} else {
-						reject('Invalid contract address.')
-					}
-				}
-			)
+		return new Promise((resolve) => {
+			contract.on('Create', async (_: string, propertyAddress: string) => {
+				resolve(propertyAddress)
+			})
 		})
 	}
