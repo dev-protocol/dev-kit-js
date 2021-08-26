374c8c3a8cc130a898d58781720ac8a8af267f3c
k3nt0w <kento.watanabe.11@gmail.com>
2021-08-26 10:26:08 +0900

    fix: utils test

--- lib/utils/index.spec.ts
+++ lib/utils/index.spec.ts
@@ -1,17 +1,11 @@
 import { DevkitUtils, utils } from './index'
 import { execute } from './execute'
-import { getAccount } from './getAccount'
-import { txPromisify } from './txPromisify'
-import { watchEvent } from './watchEvent'
 
 describe('index.ts', () => {
 	describe('utils', () => {
 		it('check return object', () => {
 			const expected: DevkitUtils = {
 				execute,
-				getAccount,
-				txPromisify,
-				watchEvent,
 			}
 
 			const result = utils
