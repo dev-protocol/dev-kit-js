ad12f690e9661a10aa7ad034532e12b23aa263a7
aggre <hiroyuki.aggre@gmail.com>
2021-09-21 10:30:26 +0900

    add dummy address

--- lib/addresses.ts
+++ lib/addresses.ts
@@ -2,9 +2,11 @@ export const addresses = {
 	eth: {
 		main: {
 			registry: '0x1D415aa39D647834786EB9B5a333A50e9935b796',
+			sTokens: '0x0000000000000000000000000000000000000000',
 		},
 		ropsten: {
 			registry: '0xD6D07f1c048bDF2B3d5d9B6c25eD1FC5348D0A70',
+			sTokens: '0x0000000000000000000000000000000000000000',
 		},
 	},
 } as const
