6d1f5fb986f10b0cd59e6a01f9c7da88f7846b7e
aggre <hiroyuki.aggre@gmail.com>
2021-10-21 21:36:21 +0900

    fix interfaces for L2

--- lib/l2/registry/index.ts
+++ lib/l2/registry/index.ts
@@ -1,4 +1,27 @@
-export {
-	createRegistryContract,
-	RegistryContract,
-} from '../../ethereum/registry'
+import { ethers } from 'ethers'
+import { Provider } from '@ethersproject/abstract-provider'
+import { Signer } from '@ethersproject/abstract-signer'
+import { addressRegistryAbi } from './abi'
+import { createRegistriesCaller } from './registries'
+
+export type RegistryContract = {
+	readonly registries: (key: string) => Promise<string>
+}
+
+export type CreateRegistryContract = (
+	provider: Provider | Signer
+) => (address: string) => RegistryContract
+
+export const createRegistryContract: CreateRegistryContract =
+	(provider: Provider | Signer) =>
+	(address: string): RegistryContract => {
+		const contract = new ethers.Contract(
+			address,
+			[...addressRegistryAbi],
+			provider
+		)
+
+		return {
+			registries: createRegistriesCaller(contract),
+		}
+	}
