5bbdfdaa073612f3927c1fcee639f0592127f17d
0xkenta <kenta0558kiichi614@gmail.com>
2021-12-14 00:11:44 +0100

    add getCreateLockupContract and positionsCreate

--- lib/agent/positionsCreate.ts
+++ lib/agent/positionsCreate.ts
@@ -1,13 +1,14 @@
-import { ethers } from 'ethers'
+import { Contract, ethers } from 'ethers'
 import { Provider } from '@ethersproject/abstract-provider'
 import { Signer } from '@ethersproject/abstract-signer'
 import { TransactionResponse } from '@ethersproject/abstract-provider'
 import { executePositionCreate, FallbackableOverrides } from './../common/utils/execute'
-import { createLockupContract } from './../ethereum/lockup/index'
+import { createLockupContract, LockupContract } from './../ethereum/lockup/index'
 import { createDepositToPropertyCaller } from './../ethereum/lockup/depositToProperty'
 
 export type Options = {
-    readonly provider: Provider | Signer,
+    readonly provider: Provider,
+    // readonly provider: Provider,
     readonly propertyAddress: string,
     readonly amount: string,
     readonly overrides?: FallbackableOverrides
@@ -15,10 +16,34 @@ export type Options = {
 
 export type PositionsCreate = (options: Options) => Promise<TransactionResponse>
 
-export const positionsCreate: PositionsCreate = (options: Options): Promise<TransactionResponse> => {
-	// cashing with provider
+export const getCreateLockupContract = async (chainId: number, provider: Provider): Promise<(address: string) => LockupContract> => {
+	const cache = new WeakMap();
+    const contract = {contract: "Lockup"}
 
-	const lockupContract = createLockupContract(options.provider)
-	return createDepositToPropertyCaller(lockupContract)
+	// eslint-disable-next-line functional/no-conditional-statement
+	if (cache.has(contract)) {
+		return cache.get(contract)
+	} else {
+        const lockupContract = chainId === 1 || chainId === 3 ?
+	        createLockupContract(provider) : createLockupContract(provider) // we do not have function for L2.
+
+		// eslint-disable-next-line
+		cache.set(contract, lockupContract)
+		return lockupContract
+	}
+}
+
+export const positionsCreate: PositionsCreate = async (options: Options): Promise<TransactionResponse> => {
+    const chainId = (await options.provider.getNetwork()).chainId
+
+	const createLockupContract= await getCreateLockupContract(chainId, options.provider)
+	const lockupAddress = "0x0"
+
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	const lockupContract = await createLockupContract(lockupAddress) as any
+
+	const caller = createDepositToPropertyCaller(lockupContract)
+	const transactionResponse = await caller(options.propertyAddress, options.amount)
+	return transactionResponse
 }
 
