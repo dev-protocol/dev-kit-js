5996da5d15b7d389427b155d8fc210db0b0d9caa
0xkenta <kenta0558kiichi614@gmail.com>
2021-12-20 22:56:47 +0100

    rename files,split getContractAddress and add isMainNet()

---
+++ lib/agent/common/utils.spec.ts
@@ -0,0 +1,79 @@
+import { getL1ContractAddress, getL2ContractAddress, isMainNet } from './utils'
+import { addresses } from '../../addresses'
+import { createRegistryContract } from '../../ethereum/registry/index'
+import { homestead, ropsten, arbOne, arbRinkeby } from './const'
+
+describe('getContractAddress.ts', () => {
+	describe('isMainNet', () => {
+		it('return true if chainId is 1', async () => {
+			const resutl = await isMainNet(homestead)
+			expect(resutl).toEqual(true)
+		})
+		it('return true if chainId is 3', async () => {
+			const resutl = await isMainNet(ropsten)
+			expect(resutl).toEqual(true)
+		})
+		it('return false if chainId is 42161', async () => {
+			const resutl = await isMainNet(arbOne)
+			expect(resutl).toEqual(false)
+		})
+		it('return false if chainId is 42161', async () => {
+			const resutl = await isMainNet(arbRinkeby)
+			expect(resutl).toEqual(false)
+		})
+	})
+
+	describe('getL1ContractAddress', () => {
+		it('chainId is 1 with lockup contract', async () => {
+			const result = await getL1ContractAddress(homestead, 'lockup')
+			const expected = await createRegistryContract(homestead)(
+				addresses.eth['main'].registry
+			).lockup()
+			expect(result).toEqual(expected)
+		})
+		it('chainId is 1 with withdraw', async () => {
+			const result = await getL1ContractAddress(homestead, 'withdraw')
+			const expected = await createRegistryContract(homestead)(
+				addresses.eth['main'].registry
+			).withdraw()
+			expect(result).toEqual(expected)
+		})
+		it('chainId is 3 with lockup', async () => {
+			const result = await getL1ContractAddress(ropsten, 'lockup')
+			const expected = await createRegistryContract(ropsten)(
+				addresses.eth['ropsten'].registry
+			).lockup()
+			expect(result).toEqual(expected)
+		})
+		it('chainId is 3 with token', async () => {
+			const result = await getL1ContractAddress(ropsten, 'token')
+			const expected = await createRegistryContract(ropsten)(
+				addresses.eth['ropsten'].registry
+			).token()
+			expect(result).toEqual(expected)
+		})
+	})
+
+	describe('getL2ContractAddress', () => {
+		it('chainId is 42161 with lockup', async () => {
+			const result = await getL2ContractAddress(arbOne, 'lockup')
+			const expected = addresses.arbitrum.one.lockup
+			expect(result).toEqual(expected)
+		})
+		it('chainId is 42161 with policyFactroy', async () => {
+			const result = await getL2ContractAddress(arbOne, 'policyFactory')
+			const expected = addresses.arbitrum.one.policyFactory
+			expect(result).toEqual(expected)
+		})
+		it('chainId is 421611', async () => {
+			const result = await getL2ContractAddress(arbRinkeby, 'lockup')
+			const expected = addresses.arbitrum.rinkeby.lockup
+			expect(result).toEqual(expected)
+		})
+		it('chainId is 421611 with sTokens', async () => {
+			const result = await getL2ContractAddress(arbRinkeby, 'sTokens')
+			const expected = addresses.arbitrum.rinkeby.sTokens
+			expect(result).toEqual(expected)
+		})
+	})
+})
---
+++ lib/agent/common/utils.ts
@@ -0,0 +1,77 @@
+import { networks } from './const'
+import { addresses } from '../../addresses'
+import {
+	createRegistryContract,
+	RegistryContract,
+} from '../../ethereum/registry/index'
+import { Provider } from '@ethersproject/abstract-provider'
+
+export const isMainNet = async (provider: Provider): Promise<boolean> => {
+	const chainId = (await provider.getNetwork()).chainId
+
+	// eslint-disable-next-line functional/no-conditional-statement
+	if (
+		chainId === networks.ethereum.main ||
+		chainId === networks.ethereum.ropsten
+	) {
+		return true
+	}
+	return false
+}
+
+export const getL1ContractAddress = async (
+	provider: Provider,
+	contract: any
+): Promise<string> => {
+	const chainId = (await provider.getNetwork()).chainId
+	const registry = await createRegistryContract(provider)
+
+	const key: keyof RegistryContract = contract
+
+	const lockupAddress =
+		chainId === networks.ethereum.main
+			? registry(addresses.eth['main'].registry)[key]()
+			: registry(addresses.eth['ropsten'].registry)[key]()
+
+	return lockupAddress
+}
+
+export const getL2ContractAddress = async (
+	provider: Provider,
+	contract: any
+): Promise<string> => {
+	const chainId = (await provider.getNetwork()).chainId
+
+	const arbOneKey: keyof typeof addresses.arbitrum.one = contract
+	const arbRinkebyKey: keyof typeof addresses.arbitrum.rinkeby = contract
+
+	const lockupAddress =
+		chainId === networks.arbitrum.one
+			? addresses.arbitrum.one[arbOneKey]
+			: addresses.arbitrum.rinkeby[arbRinkebyKey]
+
+	return lockupAddress
+}
+
+// export const getContractAddress = async (
+// 	provider: Provider,
+// 	contract: any
+// ): Promise<string> => {
+// 	const chainId = (await provider.getNetwork()).chainId
+// 	const registry = await createRegistryContract(provider)
+
+// 	const key: keyof RegistryContract = contract
+// 	const arbOneKey: keyof typeof addresses.arbitrum.one = contract
+// 	const arbRinkebyKey: keyof typeof addresses.arbitrum.rinkeby = contract
+
+// 	const lockupAddress =
+// 		chainId === networks.ethereum.main
+// 			? registry(addresses.eth['main'].registry)[key]()
+// 			: chainId === networks.ethereum.ropsten
+// 			? registry(addresses.eth['ropsten'].registry)[key]()
+// 			: chainId === networks.arbitrum.one
+// 			? addresses.arbitrum.one[arbOneKey]
+// 			: addresses.arbitrum.rinkeby[arbRinkebyKey]
+
+// 	return lockupAddress
+// }
