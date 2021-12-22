c6dfd2913f2996193f6b4425ce976f3923fa667a
0xkenta <kenta0558kiichi614@gmail.com>
2021-12-22 23:57:21 +0100

    add tests for getL1ContractAddress and getL2ContractAddress that returns null

--- lib/agent/common/utils.spec.ts
+++ lib/agent/common/utils.spec.ts
@@ -1,7 +1,7 @@
 import { getL1ContractAddress, getL2ContractAddress, isL1 } from './utils'
 import { addresses } from '../../addresses'
 import { createRegistryContract } from '../../ethereum/registry/index'
-import { homestead, ropsten, arbOne, arbRinkeby } from './const'
+import { testProviders } from './const'
 
 describe('getContractAddress.ts', () => {
 	describe('isL1', () => {
@@ -24,55 +24,86 @@ describe('getContractAddress.ts', () => {
 	})
 
 	describe('getL1ContractAddress', () => {
-		it('chainId is 1 with lockup contract', async () => {
-			const result = await getL1ContractAddress(homestead, 'lockup')
-			const expected = await createRegistryContract(homestead)(
+		it('return mainnet lockup contract address', async () => {
+			const result = await getL1ContractAddress(
+				testProviders.homestead,
+				'lockup'
+			)
+			const expected = await createRegistryContract(testProviders.homestead)(
 				addresses.eth['main'].registry
 			).lockup()
 			expect(result).toEqual(expected)
 		})
-		it('chainId is 1 with withdraw', async () => {
-			const result = await getL1ContractAddress(homestead, 'withdraw')
-			const expected = await createRegistryContract(homestead)(
+		it('return mainnet withdraw contract address', async () => {
+			const result = await getL1ContractAddress(
+				testProviders.homestead,
+				'withdraw'
+			)
+			const expected = await createRegistryContract(testProviders.homestead)(
 				addresses.eth['main'].registry
 			).withdraw()
 			expect(result).toEqual(expected)
 		})
-		it('chainId is 3 with lockup', async () => {
-			const result = await getL1ContractAddress(ropsten, 'lockup')
-			const expected = await createRegistryContract(ropsten)(
+		it('return ropsten lockup contract address', async () => {
+			const result = await getL1ContractAddress(testProviders.ropsten, 'lockup')
+			const expected = await createRegistryContract(testProviders.ropsten)(
 				addresses.eth['ropsten'].registry
 			).lockup()
 			expect(result).toEqual(expected)
 		})
-		it('chainId is 3 with token', async () => {
-			const result = await getL1ContractAddress(ropsten, 'token')
-			const expected = await createRegistryContract(ropsten)(
+		it('return ropsten token contract address', async () => {
+			const result = await getL1ContractAddress(testProviders.ropsten, 'token')
+			const expected = await createRegistryContract(testProviders.ropsten)(
 				addresses.eth['ropsten'].registry
 			).token()
+			expect(result).toEqual(expected)
+		})
+		it('return null if network neither mainnet nor ropsten', async () => {
+			const result = await getL1ContractAddress(
+				testProviders.polyMumbai,
+				'lockup'
+			)
+			const expected = null
 			expect(result).toEqual(expected)
 		})
 	})
 
 	describe('getL2ContractAddress', () => {
 		it('chainId is 42161 with lockup', async () => {
-			const result = await getL2ContractAddress(arbOne, 'lockup')
+			const result = await getL2ContractAddress(testProviders.arbOne, 'lockup')
 			const expected = addresses.arbitrum.one.lockup
 			expect(result).toEqual(expected)
 		})
 		it('chainId is 42161 with policyFactroy', async () => {
-			const result = await getL2ContractAddress(arbOne, 'policyFactory')
+			const result = await getL2ContractAddress(
+				testProviders.arbOne,
+				'policyFactory'
+			)
 			const expected = addresses.arbitrum.one.policyFactory
 			expect(result).toEqual(expected)
 		})
 		it('chainId is 421611', async () => {
-			const result = await getL2ContractAddress(arbRinkeby, 'lockup')
+			const result = await getL2ContractAddress(
+				testProviders.arbRinkeby,
+				'lockup'
+			)
 			const expected = addresses.arbitrum.rinkeby.lockup
 			expect(result).toEqual(expected)
 		})
 		it('chainId is 421611 with sTokens', async () => {
-			const result = await getL2ContractAddress(arbRinkeby, 'sTokens')
+			const result = await getL2ContractAddress(
+				testProviders.arbRinkeby,
+				'sTokens'
+			)
 			const expected = addresses.arbitrum.rinkeby.sTokens
+			expect(result).toEqual(expected)
+		})
+		it('return null if network neither mainnet nor ropsten', async () => {
+			const result = await getL2ContractAddress(
+				testProviders.polyMumbai,
+				'lockup'
+			)
+			const expected = null
 			expect(result).toEqual(expected)
 		})
 	})
