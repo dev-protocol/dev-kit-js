3ea8066545ce5596fbe79b52bdbe611e36480eef
0xkenta <kenta0558kiichi614@gmail.com>
2022-01-05 00:18:35 +0900

    change return value to undefined

--- lib/agent/lockup/positionsCreate.ts
+++ lib/agent/lockup/positionsCreate.ts
@@ -2,9 +2,11 @@ import { TransactionResponse } from '@ethersproject/abstract-provider'
 import { getLockupContract } from './common'
 import { Provider } from '@ethersproject/abstract-provider'
 import { FallbackableOverrides } from '../../common/utils/execute'
+import { Wallet } from 'ethers'
+
 
 export type Options = {
-	readonly provider: Provider
+	readonly wallet: Wallet
 	readonly propertyAddress: string
 	readonly amount: string
 	readonly overrides?: FallbackableOverrides
@@ -12,17 +14,17 @@ export type Options = {
 
 type PositionsCreate = (
 	options: Options
-) => Promise<TransactionResponse | Error>
+) => Promise<TransactionResponse | undefined>
 
 export const positionsCreate: PositionsCreate = async (
 	options: Options
-): Promise<TransactionResponse | Error> => {
-	const lockupContract = await getLockupContract(options.provider)
+): Promise<TransactionResponse | undefined> => {
+	const lockupContract = await getLockupContract(options.wallet)
 
 	return lockupContract
 		? await lockupContract.depositToProperty(
 				options.propertyAddress,
 				options.amount
 		  )
-		: new Error('network is not valid')
+		: undefined
 }
