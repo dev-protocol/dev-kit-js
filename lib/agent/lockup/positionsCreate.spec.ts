9252afb91a5eea3630aa3f0d3669a37c7197bae2
0xkenta <kenta0558kiichi614@gmail.com>
2022-01-05 00:19:31 +0900

    add new test for positionCreate with ropsten

--- lib/agent/lockup/positionsCreate.spec.ts
+++ lib/agent/lockup/positionsCreate.spec.ts
@@ -1,48 +1,44 @@
-import { ethers } from 'ethers'
-// import { createLockupContract, LockupContract } from '../../ethereum/lockup'
-// import { createGetValueCaller } from '../../ethereum/lockup/getValue'
-// import { lockupAbi } from '../../ethereum/lockup/abi'
-// import { createGetPropertyValueCaller } from '../../ethereum/lockup/getPropertyValue'
-// import { createWithdrawCaller } from '../../ethereum/lockup/withdraw'
-// import { createCalculateWithdrawableInterestAmountCaller } from '../../ethereum/lockup/calculateWithdrawableInterestAmount'
-// import { createGetAllValueCaller } from '../../ethereum/lockup/getAllValue'
-// import { createGetStorageWithdrawalStatusCaller } from '../../ethereum/lockup/getStorageWithdrawalStatus'
-// import { createCalculateCumulativeHoldersRewardAmountCaller } from '../../ethereum/lockup/calculateCumulativeHoldersRewardAmount'
-// import { createCalculateCumulativeRewardPricesCaller } from '../../ethereum/lockup/calculateCumulativeRewardPrices'
-// import { createCalculateRewardAmountCaller } from '../../ethereum/lockup/calculateRewardAmount'
-// import { createCapCaller } from '../../ethereum/lockup/cap'
-// import { createDepositToPropertyCaller } from '../../ethereum/lockup/depositToProperty'
-// import { createDepositToPositionCaller } from '../../ethereum/lockup/depositToPosition'
-// import { createWithdrawByPositionCaller } from '../../ethereum/lockup/withdrawByPosition'
-// import { createMigrateToSTokensCaller } from '../../ethereum/lockup/migrateToSTokens'
-// import { createcalculateWithdrawableInterestAmountByPositionCaller } from '../../ethereum/lockup/calculateWithdrawableInterestAmountByPosition'
-import { stubTransactionResposeFactory } from '../../common/utils/for-test'
+import { ethers, Wallet } from 'ethers'
 import { Options, positionsCreate } from './positionsCreate'
 import { testProviders } from '../common/const'
+import { env } from '../common/env'
 
 describe('positionsCreate.ts', () => {
-	const host = 'localhost'
-	const address = '0x0000000000000000000000000000000000000000'
-	const provider = new ethers.providers.JsonRpcProvider(host)
-
-	const propertyAddress = '0x80a25ACDD0797dfCe02dA25e4a55A4a334EE51c5'
+	const propertyAddress = '0x38c4bF6cD20d157EE45553b0fAD13B0c6750b439'
 	const amount = '100'
 	const overrides = {
 		overrides: undefined,
 		fallback: undefined,
 	}
 
-	const stubTx = stubTransactionResposeFactory({})
-	it('return Error if network is not valid', async () => {
-		const options: Options = {
-			provider: testProviders.polyMumbai,
+	it("", async () => {
+		// await window.ethereum.enable()
+        const wallet = new ethers.Wallet(env.mnemonic, testProviders.ropsten)
+        console.log(wallet)
+        const options: Options = {
+			wallet,
 			propertyAddress,
 			amount,
 			overrides,
 		}
-		const result = async () => {
-			await positionsCreate(options)
+		const tx = await positionsCreate(options).then(result => result)
+		console.log(tx)
+	})
+	it('return undefined if network is not valid', async () => {
+		const wallet = new ethers.Wallet(env.mnemonic, testProviders.polyMumbai)
+		const options: Options = {
+			wallet,
+			propertyAddress,
+			amount,
+			overrides,
 		}
-		expect(result).rejects.toThrow('network is not valid')
+        const tx = await positionsCreate(options).then(result => result)
+		expect(tx).toEqual(undefined)
 	})
 })
+
+// declare global {
+//     interface Window {
+//         ethereum: any;
+//     }
+// }
