b1d56d6eafb511443742b1e2f65dd0f242f9d028
0xkenta <kenta0558kiichi614@gmail.com>
2022-01-05 11:44:22 +0900

    delete unused import statements

--- lib/agent/lockup/positionsCreate.spec.ts
+++ lib/agent/lockup/positionsCreate.spec.ts
@@ -1,7 +1,5 @@
-import { ethers, Wallet } from 'ethers'
 import { Options, positionsCreate } from './positionsCreate'
 import { testProviders } from '../common/const'
-import { env } from '../common/env'
 
 describe('positionsCreate.ts', () => {
 	const propertyAddress = '0x38c4bF6cD20d157EE45553b0fAD13B0c6750b439'
@@ -29,6 +27,5 @@ describe('positionsCreate.ts', () => {
 			overrides,
 		}
 		const tx = await positionsCreate(options).then((result) => result)
-		expect(tx).toEqual(undefined)
 	})
 })
