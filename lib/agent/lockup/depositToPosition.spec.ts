bac9a71845c3b8c99aca277f9c893c452ab64087
0xkenta <kenta0558kiichi614@gmail.com>
2022-01-19 11:39:30 +0900

    modify by lint

--- lib/agent/lockup/depositToPosition.spec.ts
+++ lib/agent/lockup/depositToPosition.spec.ts
@@ -4,7 +4,7 @@ import { testProviders } from '../common/const'
 
 jest.mock('./common', () => {
 	return {
-        getLockupContract: jest.fn()
+		getLockupContract: jest.fn(),
 	}
 })
 
@@ -12,22 +12,22 @@ describe('depositToPosition.ts', () => {
 	const options = {
 		provider: testProviders.ropsten,
 		positionTokenId: '1',
-		amount: '100'
+		amount: '100',
 	}
 	it('success', async () => {
 		const expected = true
 
 		;(getLockupContract as jest.Mock).mockReturnValue({
-			depositToPosition: (positionTokenId: string, amount: string) => expected
+			depositToPosition: (positionTokenId: string, amount: string) => expected,
 		})
 
 		const result = await depositToPosition(options)
 		expect(result).toEqual(expected)
 	})
-    it('return undefined if no valid network', async () => {
+	it('return undefined if no valid network', async () => {
 		const expected = undefined
 		;(getLockupContract as jest.Mock).mockReturnValue(undefined)
-        const result = await depositToPosition(options)
+		const result = await depositToPosition(options)
 		expect(result).toEqual(expected)
 	})
 })
