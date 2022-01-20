9b3f8ace02aa6948ea95a695548fe6eb31faabc1
Stuart Kuentzel <s.w.kuentzel@gmail.com>
2022-01-20 15:23:35 +0900

    adds static market addresses

---
+++ lib/marketAddresses.ts
@@ -0,0 +1,26 @@
+export const marketAddresses = {
+	eth: {
+		main: {
+			github: '0x34A7AdC94C4D41C3e3469F98033B372cB2fAf318',
+		},
+		ropsten: {
+			github: '0x1A45BA7BB4a5A4a01Eb5330C9F2fd8af47C16eBe',
+		},
+	},
+	arbitrum: {
+		one: {
+			github: '0x84b9e407e2Ee76A641b45d007bBFd9e60a13FF7d',
+		},
+		rinkeby: {
+			github: '0x82045e7aD20dca31915896ED246bB56a3B5956d8',
+		},
+	},
+	polygon: {
+		mainnet: {
+			github: '0xa7d0A25Cb2378c935FF94863C8614367b9e85Ed8',
+		},
+		mumbai: {
+			github: '0x9edc758B0477559AB6556BCA9971B4DD892d8E28',
+		},
+	},
+} as const
