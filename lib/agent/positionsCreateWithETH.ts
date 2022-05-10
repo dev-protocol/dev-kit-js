8180004ce296bed7b0015ae1267880f95fd2c339
CryptoDogg <d.enkikairo@gmail.com>
2022-05-10 13:40:34 +0900

    merge estimate and swap

--- lib/agent/positionsCreateWithETH.ts
+++ lib/agent/positionsCreateWithETH.ts
@@ -1,3 +1,4 @@
+/* eslint-disable functional/functional-parameters */
 import { FallbackableOverrides } from '../common/utils/execute'
 import { Provider } from '@ethersproject/abstract-provider'
 import { swapClients } from './common/clients/swapClients'
@@ -5,17 +6,23 @@ import { TransactionResponse } from '@ethersproject/abstract-provider'
 
 type PositionsCreateWithETH = (options: {
 	readonly provider: Provider
-	readonly propertyAddress: string
+	readonly ethAmount: string
+	readonly destination: string
 	readonly overrides: FallbackableOverrides
-}) => Promise<TransactionResponse>
+}) => Promise<{ readonly estimation: string, readonly create: () => Promise<TransactionResponse> }>
 
 export const positionsCreateWithETH: PositionsCreateWithETH = async (options) => {
 	const l2 = await swapClients(options.provider)
 
 	return l2
-		? l2.swapEthAndStakeDevCaller(
-			options.propertyAddress,
-			options.overrides
-		)
+		? {
+			estimation: await l2.getEstimatedDevForEth(options.ethAmount),
+			create: async () => {
+				return await l2.swapEthAndStakeDevCaller(
+					options.destination,
+					options.overrides
+				)
+			}
+		}
 		: (undefined as never)
 }
