90ce32b91a08a481f60d9f1bff20acf0b597ab1d
aggre <hiroyuki.aggre@gmail.com>
2022-06-06 17:22:58 +0000

    fix typing error

--- lib/agent/common/agentAddresses.ts
+++ lib/agent/common/agentAddresses.ts
@@ -3,27 +3,31 @@ export const agentAddresses = {
 		main: {
 			swap: {
 				v2: '0xC2b299CF668974939B2CCca4eB99F0749F64a2e4',
+				v3: undefined,
 			},
 		},
 	},
 	arbitrum: {
 		one: {
 			swap: {
+				v2: undefined,
 				v3: '0xDe77B9c28D49e14164E6fcf07Bb235FEB76d4a54',
 			},
 		},
 		rinkeby: {
-			swap: { v3: '0xCb4a628D6ABd9Ac0aad0Bb6caC89Eb1EDfd33231' },
+			swap: { v2: undefined, v3: '0xCb4a628D6ABd9Ac0aad0Bb6caC89Eb1EDfd33231' },
 		},
 	},
 	polygon: {
 		mainnet: {
 			swap: {
 				v2: '0xDe77B9c28D49e14164E6fcf07Bb235FEB76d4a54',
+				v3: undefined,
 			},
 		},
 		mumbai: {
 			swap: {
+				v2: undefined,
 				v3: '0x35C7B647DDFE9b499E0130eC45820b727A262C58',
 			},
 		},
