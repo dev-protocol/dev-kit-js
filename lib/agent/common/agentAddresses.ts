c3a6c3c32735ec61114cf5f0802372024bfe56c6
aggre <hiroyuki.aggre@gmail.com>
2022-07-27 14:58:16 +0900

    update to the latest deployed contract address

--- lib/agent/common/agentAddresses.ts
+++ lib/agent/common/agentAddresses.ts
@@ -11,17 +11,17 @@ export const agentAddresses = {
 		one: {
 			swap: {
 				v2: undefined,
-				v3: '0xDe77B9c28D49e14164E6fcf07Bb235FEB76d4a54',
+				v3: '0x60Ca7E3960e5F143DdB42B19B64F65c27cAD561d',
 			},
 		},
 		rinkeby: {
-			swap: { v2: undefined, v3: '0xCb4a628D6ABd9Ac0aad0Bb6caC89Eb1EDfd33231' },
+			swap: { v2: undefined, v3: '0x4cd6B431B7A242D51B4D96aE5839a8b8f060F7A9' },
 		},
 	},
 	polygon: {
 		mainnet: {
 			swap: {
-				v2: '0xDe77B9c28D49e14164E6fcf07Bb235FEB76d4a54',
+				v2: '0xA379F35b916203112D157d4655353BB17AD81DC0',
 				v3: undefined,
 			},
 			weth: '0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619',
@@ -29,7 +29,7 @@ export const agentAddresses = {
 		mumbai: {
 			swap: {
 				v2: undefined,
-				v3: '0x35C7B647DDFE9b499E0130eC45820b727A262C58',
+				v3: '0x01C818717B5471562172f3F886E4C4dC053D6ed8',
 			},
 			weth: '0xA6FA4fB5f76172d178d61B04b0ecd319C5d1C0aa',
 		},
