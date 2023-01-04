e1760902e9936ae1dd4972ed09ff482f74fecd1c
aggre <hiroyuki.aggre@gmail.com>
2023-01-04 18:04:45 +0900

    remove old APIs

--- lib/ethereum/client/index.spec.ts
+++ lib/ethereum/client/index.spec.ts
@@ -1,14 +1,10 @@
 import { DevkitClient, client } from './index'
-import { createGetVotablePolicy } from './getVotablePolicy'
 import { createDetectSTokens } from './createDetectSTokens'
-import { getStats } from './getStats'
 
 describe('index.ts', () => {
 	describe('client', () => {
 		it('check return object', () => {
 			const expected: DevkitClient = {
-				createGetVotablePolicy,
-				getStats,
 				createDetectSTokens,
 			}
 
