6557fdfd3b1e346aba58f5449c4b16b6c8ef9f92
iyashragrawal <iyashragrawal@gmail.com>
2023-06-30 15:17:49 +0530

    feat: add simple collections export

--- lib/index.ts
+++ lib/index.ts
@@ -2,5 +2,16 @@ import { contractFactory } from './ethereum/contract'
 import { addresses } from './addresses'
 import { utils } from './common/utils'
 import { client } from './ethereum/client'
+import {
+	createSimpleCollectionsContract,
+	Image,
+} from './ethereum/simpleCollection'
 
-export default { contractFactory, addresses, utils, client }
+export default {
+	contractFactory,
+	addresses,
+	utils,
+	client,
+	createSimpleCollectionsContract,
+	Image,
+}
