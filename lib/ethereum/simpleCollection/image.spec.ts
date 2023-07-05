cb8c84835ee9f2d36059f7d36fb9fc75b07ee719
iyashragrawal <iyashragrawal@gmail.com>
2023-07-05 17:26:53 +0530

    feat: add more unit tests to simple collectiosn dev kit

--- lib/ethereum/simpleCollection/image.spec.ts
+++ lib/ethereum/simpleCollection/image.spec.ts
@@ -35,5 +35,37 @@ describe('image.spec.ts', () => {
 			const result = await caller(id, address, stakingPositions, rewards, keys)
 			expect(result).toEqual(expected)
 		})
+
+		it('call failure', async () => {
+			const error = 'error'
+
+			const id = 1
+			const address = ''
+			const stakingPositions = {
+				property: '0x0000000000000000000000000000000000000000',
+				amount: '0',
+				price: '0',
+				cumulativeReward: '0',
+				pendingReward: '0'
+			}
+			const rewards = {
+				entireReward: '0',
+				cumulativeReward: '0',
+				withdrawableReward: '0'
+			}
+			const keys = ['0x000']
+			const devContract = {
+				// eslint-disable-next-line @typescript-eslint/no-unused-vars
+				image: jest
+					.fn()
+					// eslint-disable-next-line @typescript-eslint/no-unused-vars
+					.mockImplementation(async (id: number, address: string, stakingPositions: StakingPosition, rewards: Rewards, keys: readonly string[]) => Promise.reject(error)),
+			}
+			// eslint-disable-next-line @typescript-eslint/no-explicit-any
+			const caller = createQueryCaller(devContract as any, 'image')
+			const result = await caller(id, address, stakingPositions, rewards, keys).catch((err) => err)
+
+			expect(result).toEqual(error)
+		})
 	})
 })
