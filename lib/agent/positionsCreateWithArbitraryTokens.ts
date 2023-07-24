0173b2f2bd25358437ebef922320c46e3fc017ae
aggre <hiroyuki.aggre@gmail.com>
2023-07-24 19:43:19 +0900

    Improve interface of positionsCreateWithArbitraryTokens

--- lib/agent/positionsCreateWithArbitraryTokens.ts
+++ lib/agent/positionsCreateWithArbitraryTokens.ts
@@ -1,7 +1,7 @@
 /* eslint-disable functional/functional-parameters */
 import { FallbackableOverrides } from '../common/utils/execute'
 import { clientsUtilsSwapTokensForStake } from './common/clients/clientsUtilsSwapTokensForStake'
-import { ZeroAddress, type ContractRunner } from 'ethers'
+import { ZeroAddress, type ContractRunner, TransactionResponse } from 'ethers'
 import {
 	approveIfNeeded,
 	ApproveIfNeededResult,
@@ -13,7 +13,7 @@ type Params = {
 	readonly mintTo: string
 	readonly destination: string
 	readonly path: readonly (string | bigint)[]
-	readonly token?: string
+	readonly token?: undefined
 	readonly tokenAmount?: string
 	readonly devAmount?: string
 	readonly devAmountOut?: string
@@ -25,140 +25,159 @@ type Params = {
 	readonly gatewayBasisPoints?: number
 }
 
-type PositionsCreateWithArbitraryTokens = (options: Params) => Promise<{
+type ParamsWithToken = Omit<Params, 'token'> & {
+	readonly token: string
+}
+
+type PositionsCreateWithArbitraryTokens = <T extends Params | ParamsWithToken>(
+	options: T,
+) => Promise<{
 	readonly estimatedDev: string
 	readonly estimatedTokens: string
-	readonly create: () => Promise<UndefinedOr<ApproveIfNeededResult>>
+	readonly create: () => T extends Params
+		? Promise<TransactionResponse>
+		: T extends ParamsWithToken
+		? Promise<UndefinedOr<ApproveIfNeededResult>>
+		: never
 }>
 
-export const positionsCreateWithArbitraryTokens: PositionsCreateWithArbitraryTokens =
-	async (options) => {
-		const [, cont] = await clientsUtilsSwapTokensForStake(options.provider)
-		const useERC20 = options.token && options.token !== ZeroAddress
+const hasTokens = (
+	options: Params | ParamsWithToken,
+): options is ParamsWithToken =>
+	options.token !== undefined && options.token !== ZeroAddress
 
-		return cont
-			? {
-					estimatedDev: options.tokenAmount
-						? await cont.getEstimatedDevForTokens(
-								options.path,
-								options.tokenAmount,
-						  )
-						: 'No tokenAmount provided',
-					estimatedTokens: options.devAmount
+export function positionsCreateWithArbitraryTokens(
+	opts: ParamsWithToken,
+): Promise<
+	UndefinedOr<{
+		readonly estimatedDev: string
+		readonly estimatedTokens: string
+		readonly create: () => Promise<ApproveIfNeededResult>
+	}>
+>
+export function positionsCreateWithArbitraryTokens(opts: Params): Promise<
+	UndefinedOr<{
+		readonly estimatedDev: string
+		readonly estimatedTokens: string
+		readonly create: () => Promise<TransactionResponse>
+	}>
+>
+
+export async function positionsCreateWithArbitraryTokens(
+	options: ParamsWithToken | Params,
+): Promise<unknown> {
+	const [, cont] = await clientsUtilsSwapTokensForStake(options.provider)
+	const useERC20 = hasTokens(options)
+
+	return cont
+		? {
+				estimatedDev: options.tokenAmount
+					? await cont.getEstimatedDevForTokens(
+							options.path,
+							options.tokenAmount,
+					  )
+					: 'No tokenAmount provided',
+				estimatedTokens: options.devAmount
+					? await cont.getEstimatedTokensForDev(options.path, options.devAmount)
+					: 'No devAmount provided',
+				create: async () => {
+					const tokenAmount = options.tokenAmount
+						? options.tokenAmount
+						: options.devAmount
 						? await cont.getEstimatedTokensForDev(
 								options.path,
 								options.devAmount,
 						  )
-						: 'No devAmount provided',
-					create: async () => {
-						const tokenAmount = options.tokenAmount
-							? options.tokenAmount
-							: options.devAmount
-							? await cont.getEstimatedTokensForDev(
-									options.path,
-									options.devAmount,
-							  )
-							: 'Neither tokenAmount nor devAmount provided'
-						const _overrides = {
-							overrides: {
-								...{ value: useERC20 ? undefined : tokenAmount },
-								...options.overrides?.overrides,
-							},
-						}
+						: 'Neither tokenAmount nor devAmount provided'
+					const _overrides = {
+						overrides: {
+							...{ value: useERC20 ? undefined : tokenAmount },
+							...options.overrides?.overrides,
+						},
+					}
 
-						const devAmountOut = options.devAmountOut
-							? options.devAmountOut
-							: options.tokenAmount
-							? await cont.getEstimatedDevForTokens(
-									options.path,
-									options.tokenAmount,
-							  )
-							: 'Neither devAmountOut nor tokenAmount provided'
-						const deadline = options.deadline
-							? options.deadline
-							: ((await options.provider.provider?.getBlock('latest'))
-									?.timestamp ?? Math.floor(new Date().getTime() / 1000)) + 300
+					const devAmountOut = options.devAmountOut
+						? options.devAmountOut
+						: options.tokenAmount
+						? await cont.getEstimatedDevForTokens(
+								options.path,
+								options.tokenAmount,
+						  )
+						: 'Neither devAmountOut nor tokenAmount provided'
+					const deadline = options.deadline
+						? options.deadline
+						: ((await options.provider.provider?.getBlock('latest'))
+								?.timestamp ?? Math.floor(new Date().getTime() / 1000)) + 300
 
-						return useERC20
-							? whenDefined(options.from, async (from) => {
-									return approveIfNeeded({
-										provider: options.provider,
-										requiredAmount: tokenAmount,
-										from,
-										token: options.token,
-										to: await cont.contract().getAddress(),
-										callback: async () => {
-											return options.gatewayAddress &&
-												typeof options.gatewayBasisPoints === 'number'
-												? cont.swapTokensAndStakeDev(
-														options.mintTo,
-														options.path,
-														options.destination,
-														devAmountOut,
-														deadline,
-														tokenAmount,
-														options.payload,
-														options.token,
-														options.gatewayAddress,
-														options.gatewayBasisPoints.toString(),
-														_overrides,
-												  )
-												: cont.swapTokensAndStakeDev(
-														options.mintTo,
-														options.path,
-														options.destination,
-														devAmountOut,
-														deadline,
-														tokenAmount,
-														options.payload,
-														options.token,
-														undefined,
-														undefined,
-														_overrides,
-												  )
-										},
-									})
-							  })
-							: {
-									approvalNeeded: false,
-									approveIfNeeded: async () => {
-										return {
-											waitNeeded: false,
-											waitOrSkipApproval: async () => ({
-												run: () =>
-													options.gatewayAddress &&
-													typeof options.gatewayBasisPoints === 'number'
-														? cont.swapTokensAndStakeDev(
-																options.mintTo,
-																options.path,
-																options.destination,
-																devAmountOut,
-																deadline,
-																tokenAmount,
-																options.payload,
-																undefined,
-																options.gatewayAddress,
-																options.gatewayBasisPoints.toString(),
-																_overrides,
-														  )
-														: cont.swapTokensAndStakeDev(
-																options.mintTo,
-																options.path,
-																options.destination,
-																devAmountOut,
-																deadline,
-																tokenAmount,
-																options.payload,
-																undefined,
-																undefined,
-																undefined,
-																_overrides,
-														  ),
-											}),
-										}
+					return useERC20
+						? whenDefined(options.from, async (from) => {
+								return approveIfNeeded({
+									provider: options.provider,
+									requiredAmount: tokenAmount,
+									from,
+									token: options.token,
+									to: await cont.contract().getAddress(),
+									callback: async () => {
+										return options.gatewayAddress &&
+											typeof options.gatewayBasisPoints === 'number'
+											? cont.swapTokensAndStakeDev(
+													options.mintTo,
+													options.path,
+													options.destination,
+													devAmountOut,
+													deadline,
+													tokenAmount,
+													options.payload,
+													options.token,
+													options.gatewayAddress,
+													options.gatewayBasisPoints.toString(),
+													_overrides,
+											  )
+											: cont.swapTokensAndStakeDev(
+													options.mintTo,
+													options.path,
+													options.destination,
+													devAmountOut,
+													deadline,
+													tokenAmount,
+													options.payload,
+													options.token,
+													undefined,
+													undefined,
+													_overrides,
+											  )
 									},
-							  }
-					},
-			  }
-			: (undefined as never)
-	}
+								})
+						  })
+						: options.gatewayAddress &&
+						  typeof options.gatewayBasisPoints === 'number'
+						? cont.swapTokensAndStakeDev(
+								options.mintTo,
+								options.path,
+								options.destination,
+								devAmountOut,
+								deadline,
+								tokenAmount,
+								options.payload,
+								undefined,
+								options.gatewayAddress,
+								options.gatewayBasisPoints.toString(),
+								_overrides,
+						  )
+						: cont.swapTokensAndStakeDev(
+								options.mintTo,
+								options.path,
+								options.destination,
+								devAmountOut,
+								deadline,
+								tokenAmount,
+								options.payload,
+								undefined,
+								undefined,
+								undefined,
+								_overrides,
+						  )
+				},
+		  }
+		: undefined
+}
