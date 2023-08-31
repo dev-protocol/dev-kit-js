1a0f9a215db7d2b5e3c7a518ad4efa5925d765ac
aggre <hiroyuki.aggre@gmail.com>
2023-08-31 12:35:28 +0900

    add export: arrayify

--- lib/common/utils/index.spec.ts
+++ lib/common/utils/index.spec.ts
@@ -1,11 +1,13 @@
 import { DevkitUtils, utils } from './index'
 import { execute } from './execute'
+import { arrayify } from './arrayify'
 
 describe('index.ts', () => {
 	describe('utils', () => {
 		it('check return object', () => {
 			const expected: DevkitUtils = {
 				execute,
+				arrayify,
 			}
 
 			const result = utils
--- lib/common/utils/index.ts
+++ lib/common/utils/index.ts
@@ -1,9 +1,14 @@
 import { execute } from './execute'
+import { arrayify } from './arrayify'
 
 export type DevkitUtils = {
 	readonly execute: typeof execute
+	readonly arrayify: typeof arrayify
 }
 
 export const utils: DevkitUtils = {
 	execute,
+	arrayify,
 }
+
+export { execute, arrayify }
